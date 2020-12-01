3ece77b87679ac27f28d8619b93c15043560ae18
lhscompsci <sam.black@lubbockisd.org>
2020-11-30 23:58:05 -0600

    first attempt

--- src/com/wtxcompsci/Main.java
+++ src/com/wtxcompsci/Main.java
@@ -1,8 +1,31 @@
 package com.wtxcompsci;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Scanner infile = new Scanner(new File("input.dat"));
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        long answer = 0;
+
+        while (infile.hasNext()){
+            list.add(infile.nextInt());
+        }
+        for(int i = 0; i < list.size(); i++)
+            for(int j = i+1; j<list.size(); j++)
+                for(int k = j+1; k<list.size(); k++)
+                    if(list.get(i)+list.get(j)+list.get(k)==2020) {
+                        answer = list.get(i) * list.get(j) + list.get(k);
+                        break;
+                    }
+
+        System.out.println(answer);
+
     }
 }
