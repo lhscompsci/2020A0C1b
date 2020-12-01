3522f9789e07a7812c68017d6df6493b54543359
lhscompsci <sam.black@lubbockisd.org>
2020-12-01 00:13:51 -0600

    found the typo.  Gotta love 'em!'

--- src/com/wtxcompsci/Main.java
+++ src/com/wtxcompsci/Main.java
@@ -10,18 +10,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Scanner infile = new Scanner(new File("input.dat"));
 
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Long> list = new ArrayList<>();
 
         long answer = 0;
 
         while (infile.hasNext()){
-            list.add(infile.nextInt());
+            list.add(infile.nextLong());
         }
         for(int i = 0; i < list.size(); i++)
             for(int j = i+1; j<list.size(); j++)
                 for(int k = j+1; k<list.size(); k++)
-                    if(list.get(i)+list.get(j)+list.get(k)==2020) {
-                        answer = list.get(i) * list.get(j) + list.get(k);
+                    if((list.get(i)+list.get(j)+list.get(k))==2020L) {
+                        answer = list.get(i) * list.get(j) * list.get(k);
                         break;
                     }
 
